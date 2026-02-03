ad3d2deb11476fce4a1a14f980feb16dcf4cedcc
Júlio <jc.5047792@gmail.com>
2026-02-03 17:00:30 -0400

    Progress

--- myshell.c
+++ myshell.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
 #include <unistd.h>
-#define BUFF_SIZE 1000
+#define BUFF_SIZE 4040
 
- int execute (char * program, char * const argv[])
+ void greetings ()
 {
 
-    int n = execve(program, argv);
-
-    (n == -1) ? return 0 : return 1;
+    printf("MYSHELL RUNNING\n\n");
 
 };
 
- void parser (char* command, char programName[], char * const argv[])
+ int execute (char * program, char * argv[])
+{
+
+    int n = execve(program, argv, NULL);
+
+    if (n == -1) return 0;
+
+    return 1;
+
+};
+
+ void parser (char* command, char programName[], char * argv[])
 {
 
     int programNameDone = 0;
     int i;
     int charFound = 0;
-
+printf("parser executing...\n");
     for (i = 0; command[i] != '\n'; i++)
     {
 
@@ -67,16 +76,51 @@
         }
 
     }
+printf("parser executed\n");
 
 };
+
+ ssize_t reader (char * buff)
+{
+
+    ssize_t s = read(0, buff, sizeof(buff));
+printf("reader executed\n");
+    return s;
+
+};
+
+ void shell ()
+{
+
+    char programName[BUFF_SIZE];
+    char * argv[BUFF_SIZE];
+
+    char buff[BUFF_SIZE];
+
+    greetings();
+
+    while (1)
+    {
+
+        printf("~$");
+
+        reader(buff);
+
+        parser(buff, programName, argv);
+
+        int n = execute(programName, argv);
+
+        if (n == -1) printf("Invalid program name: %s\n", programName);
+
+    }
+
+};
+
 
  int main ()
 {
 
-    char programName[BUFF_SIZE];
-    char * argv[BUFF_SIZE][BUFF_SIZE];
-
-    
+    shell();
 
     return 0;
 
