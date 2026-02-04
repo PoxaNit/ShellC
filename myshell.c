284b288ebe23322643f7502151a1c20cb0a31e1c
Júlio <jc.5047792@gmail.com>
2026-02-04 17:59:05 -0400

    Progress

--- myshell.c
+++ myshell.c
@@ -5,15 +5,33 @@
  void greetings ()
 {
 
-    printf("MYSHELL RUNNING\n\n");
+    printf("MYSHELL RUNNING\n");
 
 };
 
+void leftTrim(char *str)
+{
+    int i = 0;
+    int k = 0;
+
+    while (str[i] == ' ')
+    {
+        i++;
+    }
+
+    while (str[i] != '\0')
+    {
+        str[k++] = str[i++];
+    }
+
+    str[k] = '\0';
+}
+
  int execute (char * program, char * argv[])
 {
-
+printf("execute executing...\n");
     int n = execve(program, argv, NULL);
-
+printf("n: %i\n", n);
     if (n == -1) return 0;
 
     return 1;
@@ -22,68 +40,67 @@
 
  void parser (char* command, char programName[], char * argv[])
 {
-
-    int programNameDone = 0;
-    int i;
-    int charFound = 0;
 printf("parser executing...\n");
-    for (i = 0; command[i] != '\n'; i++)
+
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    leftTrim(command);
+
+    while (command[i] != ' ')
     {
 
-        if (!programNameDone)
-        {
-            if (command[i] != ' ')
-            {
+	programName[i] = command[i];
 
-                charFound = 1;
-
-                programName[i] = command[i];
-
-            } else {
-
-                if (charFound) programNameDone = 1;
-
-            }
-
-        } else
-        {
-
-            int j = 0;
-
-            while (command[i] != '\n')
-            {
-
-                int k = 0;
-
-                while (command[i] != ' ')
-                {
-
-                    argv[j][k] = command[i];
-
-		    k++;
-
-                    i++;
-
-                }
-
-                if (command[i - 1] != ' ') j++;
-
-		i++;
-
-
-            }
-
-        }
+        i++;
 
     }
+
+    i++;
+
+    while (command[i] != '\0')
+    {
+printf("command[i]: %c\n", command[i]);
+
+	if (command[i] != ' ')
+	{
+
+	    if (argv[j] == NULL)
+	    {
+
+		char buff[BUFF_SIZE];
+
+		argv[j] = buff;
+
+	    }
+
+	    argv[j][k] = command[i];
+
+            k++;
+
+	    i++;
+
+	    continue;
+
+	}
+printf("arg: %s\n", argv[j]);
+
+
+
+        if (command[i - 1] != ' ') { k = 0; j++; }
+	i++;
+
+    }
+
 printf("parser executed\n");
 
 };
 
- ssize_t reader (char * buff)
+ ssize_t reader (char * buff, size_t size)
 {
 
-    ssize_t s = read(0, buff, sizeof(buff));
+    ssize_t s = read(0, buff, size);
 printf("reader executed\n");
     return s;
 
@@ -97,14 +114,16 @@ printf("reader executed\n");
 
     char buff[BUFF_SIZE];
 
+    char cli[3] = {'~', '$', '\0'};
+
     greetings();
 
-    while (1)
-    {
+//    while (1)
+  //  {
 
-        printf("~$");
+	write(0, cli, sizeof(cli));
 
-        reader(buff);
+        ssize_t s = reader(buff, sizeof(buff));
 
         parser(buff, programName, argv);
 
@@ -112,7 +131,7 @@ printf("reader executed\n");
 
         if (n == -1) printf("Invalid program name: %s\n", programName);
 
-    }
+    //}
 
 };
 
